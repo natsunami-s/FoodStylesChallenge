03d48e0c9dbf38e79986b96cf16908507ba58a7a
Natalia Suharevsky <nsuharevsky@gmail.com>
2022-05-12 00:14:38 +0200

    add sign up logic

--- src/features/auth/signUp/index.tsx
+++ src/features/auth/signUp/index.tsx
@@ -2,11 +2,13 @@ import { Keyboard } from 'react-native';
 import { withFormik } from 'formik';
 import React from 'react';
 import { useMutation } from '@apollo/client';
+import { useDispatch } from 'react-redux';
 
 import { FormProps, FormValues, TSignUpContainer } from './types';
 import { signUpSchema } from 'utils/validations';
 import SignUpScreen from './signUp';
 import { SIGN_UP_WITH_EMAIL } from 'graphql/mutations/signUpWithEmail';
+import { setUserData } from 'redux/ducks/user/reducer';
 
 const SignUpContainer: React.FC<TSignUpContainer> = ({
   navigation,
@@ -25,11 +27,20 @@ const SignUpContainer: React.FC<TSignUpContainer> = ({
     },
   });
 
+  const dispatch = useDispatch();
+
   const onSignUpPress = async () => {
     Keyboard.dismiss();
     try {
       const res = await signUpWithEmail();
       console.warn(res);
+      dispatch(
+        setUserData({
+          user: res.data.signUpWithEmail.user,
+          accessToken: res.data.signUpWithEmail.accessToken,
+          refreshToken: res.data.signUpWithEmail.refreshToken,
+        }),
+      );
     } catch (e) {
       console.warn('e', e.networkError.result.errors);
     }
